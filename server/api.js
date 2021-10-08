bf51db5e15fc963f02ea8b2c4943cd212746f15b
oma-rika <omt2266@gmail.com>
2021-10-08 15:51:07 +0900

    MySQLに接続

--- server/api.js
+++ server/api.js
@@ -8,35 +8,38 @@ const app     = express();
 app.get('/api',   (req, res) => {
     //res.status(200).send('app gets!!!');
     console.log('app.getに遷移');
-    res.set({'Access-Control-Allow-Origin': '*'});
+    //res.set({ 'Access-Control-Allow-Origin': 'http://localhost:56976' });
     const mysql = require('mysql')
     const connection = mysql.createConnection({
         host : 'localhost',
-        user:  '****',
+        user:  'root',
         database: 'albumapp',
         password: '******',
     })
     let ret=[];
-    connection.connect()
+    connection.connect((err) => {
+        if (err) throw err;
+        console.log('connected to mysql');
+    });
     let sql = 'select * from albumapp.user'
-    connection.query(sql, function(error, row, fields) {
+    connection.query(sql, function(error, rows, fields) {
         if (error) res.status(500).send(error);
         let date = [];
-        for (let i = 0; i < row.length; i++) {
+        for (let i = 0; i < rows.length; i++) {
             console.log(rows[i]);
             date.push({
-                id: row[i].id,
-                Account: row[i].Account
+                id: rows[i].id,
+                Account: rows[i].Account
             })
         }
         ret = JSON.stringify(date);
-        res.header('Content-Type', 'application/json; charset=utf-8')
-        res.send(ret);
+        res.header('Content-Type', 'application/json; charset=utf-8');
+        res.write(ret);
     })
-    res.status(200).send(ret);
-    connection.end(ret);
-})
+    //res.status(200).send(ret);
+    connection.end();
+});
 
-app.listen(5000, () => {
+app.listen(3010, () => {
     console.log('api');
-})
+});
