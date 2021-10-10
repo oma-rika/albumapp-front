39c1a186e7afa75b9f0e78a41b521e1da0d8be32
oma-rika <omt2266@gmail.com>
2021-10-11 08:38:31 +0900

    フッター追加

---
+++ components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <footer>
+        <p class="copyright"><span>&copy;OmataR WebApp</span></p>
+    </footer>
+</template>
+<style scoped>
+    footer {
+        background-color: #eeee;
+        text-align: center;
+        color: #000;
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        padding: 15px 0;
+    }
+</style>
