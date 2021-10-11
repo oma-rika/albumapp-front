55e183588e7c302437749750c827d98167a18570
oma-rika <omt2266@gmail.com>
2021-10-11 11:02:57 +0900

    イベントテスト

--- pages/edit.vue
+++ pages/edit.vue
@@ -1,37 +1,68 @@
 <template>
     <section>
-        <header>
-            <NuxtLink to="/signin" class="signin-button">サインイン</NuxtLink>
-        </header>
-        <h1>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</h1>
-        <div class="main">
-            <img src="~/assets/cat.jpg" class="image">
-            <div class="imageDownload-icon"></div>
-            <div class="folderUpload-icon"></div>
+        <header />
+        <div class="container">
+            <h1 class="fs-5 fw-normal form-h1">Data Files Upload</h1>
+            <div class="form-group">
+                <label for="selectFile" class="form-label">取り込むデータを選択</label>
+                <input type="file" id="selectFile" class="form-control">
+            </div>
+            <div class="form-group">
+                <button @click="countUp" type="button" class="btn btn-primary">ファイル内容を表示</button>
+            </div>
+            <div class="showImageFile">
+                [{{ count }}]
+                <img id="disp" />
+            </div>
+            <div class="form-group">
+                <button type="button" class="btn btn-primary">データを取り込む</button>
+                <button type="button" class="btn btn-secondary">キャンセルする</button>
+            </div>
+
+
+            <div class="main">
+                <img src="~/assets/cat.jpg" class="image">
+                <div class="imageDownload-icon"></div>
+                <div class="folderUpload-icon"></div>
+            </div>
+            <div class="edit-action">
+                <h2 class="fs-5 fw-normal form-h2">バージョン履歴</h2>
+                <div class="session">
+                    <div class="datetime">2021/08/12(火)00:00:00</div>
+                    <div class="arrow"></div>
+                </div>
+                <div class="session">
+                    <div class="datetime">2021/08/12(火)00:00:00</div>
+                    <div class="arrow"></div>
+                </div>
+                <div class="session">
+                    <div class="datetime">2021/08/12(火)00:00:00</div>
+                    <div class="arrow"></div>
+                </div>
+            </div>
+            <button>Download</button>
         </div>
-        <div class="edit-action">
-            <p>バージョン履歴</p>
-            <div class="session">
-                <div class="datetime">2021/08/12(火)00:00:00</div>
-                <div class="arrow"></div>
-            </div>
-            <div class="session">
-                <div class="datetime">2021/08/12(火)00:00:00</div>
-                <div class="arrow"></div>
-            </div>
-            <div class="session">
-                <div class="datetime">2021/08/12(火)00:00:00</div>
-                <div class="arrow"></div>
-            </div>
-        </div>
-        <button>Download</button>
+        <Footer />
     </section>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+    data() {
+        return {
+            posts: [],
+            count: 'Hello',
+        }
+    },
+    methods: {
+        countUp (): String {
+            console.log('Hello');
+            return this.count += 'world'
+        }
+    }
+})
 </script>
 
 <style>
