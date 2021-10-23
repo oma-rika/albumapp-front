d700a61ece3746fac31d493d1edaf1821c5d09a8
oma-rika <omt2266@gmail.com>
2021-10-23 14:26:53 +0900

    編集画面更新

--- pages/edit.vue
+++ pages/edit.vue
@@ -3,20 +3,27 @@
         <header />
         <div class="container">
             <h1 class="fs-5 fw-normal form-h1">Data Files Upload</h1>
+            <!--<div class="form-group">
+                <label for="selectFile" class="form-label">取り込むデータを選択</label>
+                <input type="file" v-on:change="handleFile" id="selectFile" class="form-control">
+            </div>
+            <div class="form-group">
+                <button @click="showImageFile" type="button" class="btn btn-primary">ファイル内容を表示</button>
+            </div>-->
             <div class="form-group">
                 <label for="selectFile" class="form-label">取り込むデータを選択</label>
-                <input type="file" id="selectFile" class="form-control">
+                <input type="file" accept="image/*" @change="readImage" id="selectImage" class="form-control" />
             </div>
-            <div class="form-group">
-                <button @click="countUp" type="button" class="btn btn-primary">ファイル内容を表示</button>
+            <div class="image-preview">
+                <img src="/images/dummy.jpg" v-if="!binaryFile" alt="プレビュー画像" />
+                <img :src="binaryFile" v-if="binaryFile" alt="プレビュー画像" />
             </div>
             <div class="showImageFile">
-                [{{ count }}]
-                <img id="disp" />
+                <img id="disp" :src="binaryFile" v-if="binaryFile.length > 0" />
             </div>
             <div class="form-group">
                 <button type="button" class="btn btn-primary">データを取り込む</button>
-                <button type="button" class="btn btn-secondary">キャンセルする</button>
+                <button type="button" @click="chancelBtn" class="btn btn-secondary">キャンセルする</button>
             </div>
 
 
@@ -53,13 +60,54 @@ export default Vue.extend({
     data() {
         return {
             posts: [],
-            count: 'Hello',
+            binaryFile: '',
+            testUrl: '',
+            file: '',
         }
     },
     methods: {
-        countUp (): String {
+        //handleFile ({e}: {e:any}): void {
+        readImage (): void {
+            const inputImage = (<HTMLInputElement>document.querySelector('#selectImage'))!.files[0];
+            console.log('inputImage.files:', inputImage);
+            if (inputImage.length == 0) return;
+            const reader = new FileReader();
+            reader.readAsDataURL(inputImage);
+            reader.onload = (e) => {
+                this.binaryFile =  e.target.result;
+            }
+            reader.onerror = (e) => {
+                alert('読み取り時にエラーが発生しました');
+            }
+        },
+        chancelBtn (): void {
+            this.binaryFile = '';
+        },
+        showImageFile (): Object {
             console.log('Hello');
-            return this.count += 'world'
+            console.log('this.binaryFile:', this.binaryFile);
+            /*this.binaryFile = 'get Image';
+            const reader = new FileReader();
+            const selectFile = document.querySelector('#selectFile');
+
+            if (selectFile != null) {
+                let fileInfo = selectFile.files;
+                console.log('fileInfo:', fileInfo);
+                reader.readAsDataURL(fileInfo[0]);
+            }
+
+            reader.onerror = function(event) {
+                alert('読み取り時にエラーが発生しました');
+            };
+
+            reader.onload = function(event) {
+                console.log('get Loading');
+                console.log(event);
+                let tmpTxt = event.target.result;
+                this.binaryFile = tmpTxt;
+            };
+            console.log('77777', this.binaryFile);*/
+            return this.binaryFile;
         }
     }
 })
