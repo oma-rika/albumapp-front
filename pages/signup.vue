38153640b4f6f58ed8a7c65b97d47bdb441aed53
oma-rika <omt2266@gmail.com>
2021-11-13 18:24:51 +0900

    POSTされたデータを受け取る

--- pages/signup.vue
+++ pages/signup.vue
@@ -3,21 +3,21 @@
       <Header />
       <div class="container">
         <h1 class="fs-5 text-center fw-normal form-h1">アカウントを作成</h1>
-        <form action="/" method="post"　class="signup-form">
+        <form action="http://localhost:3010/api" method="POST"　class="signup-form">
             <div class="form-group">
               <label for="name" class="form-label">ユーザー名</label>
-              <input type="text" id="name" class="form-control" placeholder="Please enter your name">
+              <input type="text" id="name" class="form-control" placeholder="Please enter your name" v-model="name">
             </div>
             <div class="form-group">
               <label for="e-mailaddress" class="form-label">メールアドレス</label>
-              <input type="text" id="e-mailaddress" class="form-control" placeholder="Please enter your e-mail address">
+              <input type="text" id="e-mailaddress" class="form-control" placeholder="Please enter your e-mail address" v-model="mailAddress">
             </div>
             <div class="form-group">
               <label for="possword" class="form-label">パスワード</label>
-              <input type="password" id="possword" class="form-control" aria-describedby="passwordHelpBlock">
+              <input type="password" id="possword" name="password" class="form-control" aria-describedby="passwordHelpBlock" v-model="password">
             </div>
             <div class="form-group">
-                <button type="button" class="btn btn-primary btn-block">新規登録</button>
+                <button type="button" @click="signUp" class="btn btn-primary btn-block">新規登録</button>
             </div>
         </form>
       </div>
@@ -25,19 +25,30 @@
     </section>
 </template>
 
+<script lang="ts">
+import Vue from 'vue'
+import axios from 'axios'
+
 export default Vue.extend({
   data() {
     return {
-      firstname: 'foo',
+      name: '',
+      mailAddress: '',
+      password: '',
     }
   },
-  mounted() {
-    axios.get('/server/api')
-      .then(response => this.posts = response.data)
-    console.log("7777" + "[" + this.posts + "]");
-  },
   methods: {
-
+    signUp(): void {
+      console.log('signUpClick');
+      axios.post('http://localhost:3010/api', {
+        UserName: this.name,
+        email: this.mailAddress,
+        password: this.password,
+      })
+      .then(function (response) {
+        console.log('res:', response);
+      })
+    }
   }
 })
 </script>
