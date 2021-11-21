357bb7c5355894392f157a02c3283095048fdf01
oma-rika <omt2266@gmail.com>
2021-11-21 12:55:56 +0900

    add layouts folder

---
+++ layouts/Top.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <Header />
+    <div class="container">
+      <p>This is Top Page!!</p>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+</script>
+<style>
+</style>
---
+++ layouts/Welcome.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <Header />
+    <div class="container">
+      <p>This is Welcome Page!!</p>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+</script>
+<style>
+</style>
