7e84435d53f2e458304712e94f0bd8080ce8ea9b
Rika Omata <omt2266@gmail.com>
2021-12-25 12:10:08 +0900

    ログイン後のヘッダー追加

--- components/Header.vue
+++ components/Header.vue
@@ -1,20 +1,66 @@
 <template>
-    <header>
-        <NuxtLink to="/signin" class="signin-button">サインイン</NuxtLink>
-    </header>
+    <v-app-bar 
+      app
+      dark
+      dense
+      clipped-left="1"
+    >
+      <v-app-bar-nav-icon />
+      <v-toolbar-title>Page title</v-toolbar-title>
+
+      <v-spacer />
+      <v-menu
+        app
+        offset-x
+        offset-y
+        max-width="200"
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn
+            icon
+            v-on="on"
+            dark
+          >
+            <v-icon>
+              mdi-account-circle
+            </v-icon>
+          </v-btn>
+        </template>
+
+        <v-list dense>
+          <v-list-item-group>
+            <v-list-item
+              v-for="(item, i ) in items"
+              :key="i"
+            >
+              <v-list-item-icon>
+                <v-icon v-text="item.icon"></v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title v-text="item.text"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
 </template>
-<style lang="scss" scoped>
-    header {
-        background-color: #24292e;
-        padding: 5px 20px;
-        height: 40px;
-        display: flex;
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+      
+  },
+  data() {
+    return {
+      items: [
+        { text: 'ダッシュボード', icon: 'mdi-view-dashboard-outline'},
+        { text: 'アカウント設定', icon: 'mdi-account-cog-outline'},
+        { text: 'ログアウト', icon: 'mdi-logout-variant'},
+      ]
     }
-    .signin-button {
-        padding: 4px 8px;
-        border-radius: 3px;
-        border: 1px solid #fff;
-        color: #fff;
-        background: transparent;
-    }
+  },
+})
+</script>
+<style>
 </style>
