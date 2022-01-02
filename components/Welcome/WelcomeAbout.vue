71b005d3e1721aedd4f0b46277213bd727a76021
Rika Omata <omt2266@gmail.com>
2022-01-02 19:01:04 +0900

    welcomeページにコンテンツを追加

---
+++ components/Welcome/WelcomeAbout.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-row align="center">
+        <v-col cols="12">
+            <h1 class="text-center">あなたのデバイスにあるデータを<br />オンラインで簡単に共有できます</h1>
+            <p class="text-center">写真・画像を登録すれば好きな時にいつでもアクセスができます。<br>
+            グループを作成して簡単に共有することもできます。<br />
+            お気に入りの写真やファイルを保存するのに最適です。</p>
+        </v-col>
+    </v-row>
+</template>
+<script lang="ts">
+
+
+</script>
---
+++ components/Welcome/WelcomeStart.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-row align="center" class="pb-10 bg-color">
+        <v-col
+            cols="12"
+            class="text-center"
+        >
+            <h1>さっそく始めよう</h1>
+            <p>まずは会員登録！</p>
+            <v-btn
+                x-large
+                color="primary"
+                to="/createAccount"
+            >
+                新規会員登録
+            </v-btn>
+        </v-col>
+    </v-row>
+</template>
+<style lang="scss">
+.bg-color {
+    background: #f5fafb;
+}
+
+
+</style>
+<script lang="ts">
+
+
+</script>
