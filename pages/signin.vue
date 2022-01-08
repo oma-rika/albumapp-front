af51e69c2fc750d0095919cbf04bc213280676d5
Rika Omata <omt2266@gmail.com>
2022-01-08 14:02:35 +0900

    ログインページ作成中

--- pages/signin.vue
+++ pages/signin.vue
@@ -1,8 +1,73 @@
 <template>
     <section>
-        <Header />
+        <v-app>
+            <MenuBar />
+            <v-main>
+                <v-container fluid>
+                    <v-row justify="center">
+                        <v-col
+                            cols="12"
+                            sm="10"
+                            md="8"
+                            lg="6"
+                            align="center"
+                        >
+                             <v-card
+                                class="mt-12"
+                                max-width="350"
+                            >
+                                <v-card-title>
+                                    ログイン
+                                </v-card-title>
+                                [{{ $route.fullPath }}]
+                                <v-card-text class="pb-0">
+                                    <v-form
+                                        ref="form"
+                                        v-model="isValid"
+                                        @submit.prevent="login"
+                                    >
+                                        <UserFormEmail
+                                            :email.sync="params.user.email"
+                                            />
+                                        <userFormPassword
+                                            :password.sync="params.user.password"
+                                            />
+                                        <v-card-actions class="px-0 py-0">
+                                            <v-btn
+                                                type="submit"
+                                                color="primary"
+                                                x-large
+                                                block
+                                                :disabled="!isValid || loading"
+                                                :loading="loading"
+                                                class="white--text"
+                                            >
+                                                ログイン
+                                            </v-btn>
+                                        </v-card-actions>
+                                        <v-card-actions class="px-0">
+                                            <nuxt-link
+                                                to="#"
+                                                class="body-2 text-decoration-none"
+                                            >
+                                                パスワードを忘れた場合はこちら
+                                            </nuxt-link>
+                                        </v-card-actions>
+                                    </v-form>
+                                </v-card-text>
+                            </v-card>
+                            <v-card >
+
+                            </v-card>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </v-main>
+        </v-app>
+
+                                                                                
         <div class="container signinPage">
-                      <p>[{{$store.state.message}}]</p>
+            <p>[{{$store.state.message}}]</p>
             <h1 class="fs-5 text-center fw-normal form-h1">ログイン</h1>
             <form action="http://localhost:3010/login" method="post" class="signin-form">
                 <div class="form-group">
@@ -16,12 +81,12 @@
                         <NuxtLink to="/possword_reset" class="fs-6">パスワードを忘れた場合はこちら</NuxtLink>
                     </div>
                 </div>
-                <div class="form-group">
+                <!--<div class="form-group">
                     <button type="button" @click="signIn" class="btn btn-primary btn-block">Sign&nbsp;in</button>
                 </div>
                 <div class="form-group">
                     <NuxtLink to="/signup" class="signin-button">Create an account.</NuxtLink>
-                </div>
+                </div>-->
             </form>
 
             <!--<button @click="$store.dispatch('updateMessageAction', 'Dispatch Message')">ログインできました。</button>-->
@@ -41,15 +106,84 @@ import axios from 'axios'
 export default Vue.extend({
     data() {
         return {
+            isValid: false,
+            loading: false,
+            params: {
+                //TODO 削除する
+                user: {
+                    email: 'test@gmail.com',
+                    password: 'possword'
+                }
+            },
             emailAddress: '',
             passWord: '',
             auth: [
                 {token: 'dummy token'},
             ],
+            failure: false,
         }
     },
     methods: {
-        signIn(): void {
+        async login() {
+            this.loading = true;
+            if (this.isValid) {
+                await axios.post('http://localhost:3010/login', {
+                    emailAddress: this.params.user.email,
+                    passWord: this.params.user.password,
+                }).then((response) => {
+                    this.authSuccessful(response);
+                }).catch(error => {
+                    this.authFailure(error)                   
+                })
+                this.loading = false;
+            }
+            /*setTimeout(() => {
+                console.log('setLogin:');
+                if (this.isValid) {
+                    await axios.post('http://localhost:3010/login', {
+                        emailAdress: this.params.user.email,
+                        passWord: this.params.user.password,
+                    }).then((response) => {
+                        this.authSuccessful(response);
+                    }).catch(error => {
+                        this.authFailure(error)                   
+                    })
+                    this.loading = false;
+                }
+            }, 1500)*/
+        },
+        authSuccessful (response:any) {
+            console.log('ログイン成功');
+            console.log(response);
+            //TODO ログイン処理
+            switch(response.data.status) {
+                case 'ok':
+                    console.log('ok');
+                    this.auth.push(response.data.items[0]);
+                    //console.log('this.auth:', this.auth);
+                    console.log('response.data.items:', response.data.items[0]);
+                    this.$store.dispatch("updateMessageAction", response.data.items[0]);
+                    this.$store.dispatch("login")
+                    //this.auth = response.data.items[0];
+                    //成功したらリダイレクト
+                    this.$router.push('project');
+                    break;
+                default:
+                    console.log('notAccount');
+                    //20211114追加
+                    //後程修正する
+                    this.failure = true;
+                    break;
+            }
+            //TODO 記憶ルートリダイレクト
+        },
+        authFailure ({response}:{response:any}) {
+            if (response && response.status === 404) {
+                console.log('response.status:', response.status);
+            }
+            //console.log("エラー発生");
+        },
+        /*signIn(): void {
             axios.post('http://localhost:3010/login', {
                 emailAddress: this.emailAddress,
                 passWord: this.passWord,
@@ -79,7 +213,7 @@ export default Vue.extend({
                         break;
                 }
             })
-        }
+        }*/
     }
 })
 
