f17a646a2a0adc91e1cf87076629d54ea9cdb068
Rika Omata <omt2266@gmail.com>
2022-01-08 15:19:18 +0900

    ログインフォーム調整

--- pages/signin.vue
+++ pages/signin.vue
@@ -63,36 +63,8 @@
                     </v-row>
                 </v-container>
             </v-main>
+            <Footer />
         </v-app>
-
-                                                                                
-        <div class="container signinPage">
-            <p>[{{$store.state.message}}]</p>
-            <h1 class="fs-5 text-center fw-normal form-h1">ログイン</h1>
-            <form action="http://localhost:3010/login" method="post" class="signin-form">
-                <div class="form-group">
-                    <label for="e-mailaddress" class="form-label">メールアドレス</label>
-                    <input type="text" id="e-mailaddress" class="form-control" placeholder="Please enter your e-mail address" v-model="emailAddress">
-                </div>
-                <div class="form-group">
-                    <label for="possword" class="form-label">パスワード</label>
-                    <input type="password" id="possword" name="password" class="form-control" aria-describedby="passwordHelpBlock" v-model="passWord">
-                    <div id="passwordHelpBlock" class="form-text text-right">
-                        <NuxtLink to="/possword_reset" class="fs-6">パスワードを忘れた場合はこちら</NuxtLink>
-                    </div>
-                </div>
-                <!--<div class="form-group">
-                    <button type="button" @click="signIn" class="btn btn-primary btn-block">Sign&nbsp;in</button>
-                </div>
-                <div class="form-group">
-                    <NuxtLink to="/signup" class="signin-button">Create an account.</NuxtLink>
-                </div>-->
-            </form>
-
-            <!--<button @click="$store.dispatch('updateMessageAction', 'Dispatch Message')">ログインできました。</button>-->
-
-        </div>
-        <Footer />
     </section>
 </template>
 
@@ -135,22 +107,10 @@ export default Vue.extend({
                 }).catch(error => {
                     this.authFailure(error)                   
                 })
-                this.loading = false;
             }
-            /*setTimeout(() => {
-                console.log('setLogin:');
-                if (this.isValid) {
-                    await axios.post('http://localhost:3010/login', {
-                        emailAdress: this.params.user.email,
-                        passWord: this.params.user.password,
-                    }).then((response) => {
-                        this.authSuccessful(response);
-                    }).catch(error => {
-                        this.authFailure(error)                   
-                    })
-                    this.loading = false;
-                }
-            }, 1500)*/
+            setTimeout(() => {
+                this.loading = false;
+            }, 1500)
         },
         authSuccessful (response:any) {
             console.log('ログイン成功');
@@ -182,65 +142,12 @@ export default Vue.extend({
                 console.log('response.status:', response.status);
             }
             //console.log("エラー発生");
-        },
-        /*signIn(): void {
-            axios.post('http://localhost:3010/login', {
-                emailAddress: this.emailAddress,
-                passWord: this.passWord,
-            })
-            .then((response) => {
-                switch(response.data.status) {
-                    case 'ok':
-                        console.log('ok');
-                        this.auth.push(response.data.items[0]);
-                        //console.log('this.auth:', this.auth);
-                        //console.log('response.data.items:' {response.data.items[0])};
-                        this.$store.dispatch("updateMessageAction", this.auth);
-                        //this.auth = response.data.items[0];
-                        //成功したらリダイレクト
-                        window.location.href = '/';
-                        break;
-                    case 'notAccount':
-                        console.log('notAccount');
-                        //20211114追加
-                        //後程修正する
-                        let msg = document.createElement('p');
-                        msg.textContent =  '該当するアカウントはありません。もう一度お試し下さい';
-                        const element = document.querySelector<HTMLElement>('.signinPage')
-                        if (element) {
-                            element.appendChild(msg);
-                        }
-                        break;
-                }
-            })
-        }*/
+        }
     }
 })
 
 </script>
 
 <style>
-    .form-h1 {
-        padding: 30px 0 0;
-    }
-    .signin-form {
-        width: 100%;
-        max-width: 410px;
-        padding: 0 15px 15px 15px;
-        margin: auto;
-    }
-    label {
-        display: block;
-    }
 
-    .btn-block {
-        display: block;
-        width: 100%;
-        text-align: center;
-    }
-
-    .form-group {
-        margin-top: 5px;
-        margin-bottom: 15px;
-    }
 </style>
