a61ea1be23955b762553399e8661c36a47e6d241
Rika Omata <omt2266@gmail.com>
2022-01-10 17:40:11 +0900

    Vuexのストアを更新

--- store/index.js
+++ store/index.js
@@ -9,6 +9,31 @@ const createStore = () => {
                 userId: 0,
                 token: '',
                 loggedIn: false,
+                user: {
+                    current: null
+                },
+                auth: {
+                    token: null,
+                    expires: 0,
+                    payload: {}
+                },
+                project: {
+                    current: null,
+                    list: [
+                        {id: 1, name: 'MyProject01', updateAt: '2020-04-01T12:00:00+09:00'},
+                        {id: 2, name: 'MyProject02', updateAt: '2020-04-01T12:00:00+09:00'},
+                        {id: 3, name: 'MyProject03', updateAt: '2020-04-01T12:00:00+09:00'},
+                    ]
+                }
+            }
+        },
+        getters: {
+            getCurrentUserId: (state, getters) => {
+                if (state.user.current) {
+                    return state.user.current;
+                } else {
+                    console.log('ユーザーは存在しない');
+                }
             }
         },
         // mutations: {
@@ -17,7 +42,7 @@ const createStore = () => {
         //     }
         // },
         mutations: {
-            updateMessage: function(state, payload) {
+            updateMessage: (state, payload) => {
                 console.log('updateMessageに遷移!!')
                 state.token = payload[0].token;
                 state.userId = payload[1].id;
@@ -29,10 +54,36 @@ const createStore = () => {
                 }
 
             },
+            login(state) {
+                state.loggedIn = true;
+                console.log('state.loggedIn', state.loggedIn);
+            },
             logout(state) {
-               state.token = '';
-               state.loggedIn = false;
-               state.userId = 0;
+                console.log('logoutに遷移');
+                state.token = '';
+                state.loggedIn = false;
+                state.userId = 0;
+                //初期化する
+                state.user = {
+                    current: null
+                };
+            },
+            //20210107追加
+            setCurrentUser (state, payload) {
+                state.user.current = payload;
+                console.log('state.user.current', state.user.current.Account);
+            },
+            setAuthToken (state, payload) {
+                state.auth.token = payload;
+            },
+            setAuthExpires (state, payload) {
+                state.auth.expires = payload;
+            },
+            setAuthPayload (state, payload) {
+                state.auth.payload = payload;
+            },
+            setCurrentProject (state, payload) {
+                state.project.current = payload;
             }
         },
         // actions: {
@@ -43,8 +94,37 @@ const createStore = () => {
         actions: {
             updateMessageAction(context, payload) {
                 console.log('updateMessageActionに遷移!!')
-                //console.log('payload:', payload);
-                context.commit('updateMessage', payload);
+                console.log('payload:', payload);
+                //context.commit('updateMessage', payload);
+                context.commit('setCurrentUser', payload);
+                context.commit('login');
+            },
+            //20210105追加
+            //{ state, getters, commit, dispatch, rootState, rootGetters }
+            // rootState => ルート(store/index.js)のstateを取得(rootState = state)
+            //
+            //20210107追加
+            getCurrentUser ({ commit }, user) {
+                commit('setCurrentUser', user);
+            },
+            getAuthToken ({ commit }, token) {
+                commit('setAuthToken', token);
+            },
+            getAuthExpires ({ commit }, expires) {
+                expires = expires || 0;
+                commit('setAuthExpires', expires);
+            },
+            getAuthPayload ({ commit }, jwtPayload) {
+                jwtPayload = jwtPayload || {}
+                commit('setAuthPayload', jwtPayload)
+            },
+            getLogout (context) {
+                console.log('getLogoutに遷移');
+                context.commit('logout');
+            },
+            getLogin (context) {
+                console.log('getLoginに遷移');
+                context.commit('login');
             }
         },
         plugins: [createPersistedState({storage: window.sessionStorage})]
