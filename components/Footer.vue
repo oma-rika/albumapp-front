410d6153f05e1641922da902b2a104ce4ea7ca02
oma-rika <omt2266@gmail.com>
2022-02-17 10:36:37 +0900

    Footer調整

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,9 +4,28 @@
             cols="12"
             class="text-center"
         >
-            &copy;OmataR WebApp
+            {{ this.year }}&copy;OmataR WebApp
         </v-col>
     </v-footer>
 </template>
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      year: 0,
+    }
+  },
+  created() {
+    const date = new Date();
+    this.year = date.getFullYear();
+  },
+  methods: {
+
+  }
+})
+</script>
 <style lang="scss" scoped>
-</style>
+
+</styl>
