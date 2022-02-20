625da85181669b410e4bcfb884560d0dbb2af2dc
Rika Omata <omt2266@gmail.com>
2022-02-20 21:58:43 +0900

    i18nで言語切り替え処理更新

--- components/MenuBar.vue
+++ components/MenuBar.vue
@@ -1,25 +1,42 @@
 <template>
-    <v-app-bar app dark dense>
-        <v-app-bar-nav-icon />
-        <v-toolbar-title>
-          <HeaderTitle />
-        </v-toolbar-title>
-        <v-spacer />
-        <v-btn text>
-            言語
-        </v-btn>
-        <v-btn text to="/signin">
-            ログイン
-        </v-btn>
-        <v-btn
-            class="ma-2"
-            outlined
-            color="white"
-            to="/signup"
-            >
-            無料会員登録
-        </v-btn>
-    </v-app-bar>
+  <v-app-bar
+    app
+    dark 
+    dense
+  >
+      <v-app-bar-nav-icon />
+      <v-toolbar-title>
+        <HeaderTitle />
+      </v-toolbar-title>
+      <v-spacer />
+      <v-menu
+        offset-y
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn
+            v-on="on"
+            text
+          >
+              言語
+          </v-btn>
+        </template>
+        <v-list dense>
+          <v-list-item><nuxt-link :to="switchLocalePath('ja')">日本語</nuxt-link></v-list-item>
+          <v-list-item><nuxt-link :to="switchLocalePath('en')">English</nuxt-link></v-list-item>
+        </v-list>     
+      </v-menu>
+      <v-btn text to="/signin">
+          ログイン
+      </v-btn>
+      <v-btn
+          class="ma-2"
+          outlined
+          color="white"
+          to="/signup"
+          >
+          無料会員登録
+      </v-btn>
+  </v-app-bar>
 </template>
 <script lang="ts">
 import Vue from 'vue'
