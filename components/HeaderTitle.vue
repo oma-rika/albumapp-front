05b65c5bde49fdbd85f2ab5e4f65beb57be177e8
oma-rika <omt2266@gmail.com>
2022-03-09 12:49:01 +0900

    ヘッダーロゴ更新

--- components/HeaderTitle.vue
+++ components/HeaderTitle.vue
@@ -1,16 +1,18 @@
 <template>
-    <v-toolbar-title>
-        <v-img
-          :src="require(`~/assets/albumapp_logo.png`)"
-          alt="albumapp"
-          aspect-ratio="1.7"
-          contain
-          style="{
-            height: 48px;
-            width: 141px;
-          }"
-        ></v-img>
-    </v-toolbar-title>
+    <v-row class="ma-0">
+      <v-avatar
+        size="36"
+        color="purple"
+        class="mr-2"
+      >
+        <v-icon dark>
+          mdi-folder-image
+        </v-icon>
+      </v-avatar>
+      <v-toolbar-title class="hidden-xs-only">
+          Album App
+      </v-toolbar-title>
+    </v-row>
 </template>
 <script lang="ts">
 import Vue from 'vue'
@@ -18,9 +20,7 @@ import Vue from 'vue'
 export default Vue.extend({
   data() {
     return {
-      posts: [],
-      imgHeight: 48,
-      imgWidth: 141
+
     }
   },
 })
