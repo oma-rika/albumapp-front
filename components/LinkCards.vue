91866d38cbde20e9d73190204601468ee0ca5bbe
oma-rika <omt2266@gmail.com>
2022-03-11 12:02:07 +0900

    トップページ編集中

--- components/LinkCards.vue
+++ components/LinkCards.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-row 
+    <v-row
         align="center"
         class="pt-10 pb-10"
     >
@@ -9,12 +9,14 @@
             :cols="card.flex"
         >
             <v-card>
+                <!--{{ card }}-->
+                <!--{{card.FilePath | dirPath}}-->
+                <!--<v-card-subtitle v-text="{{ card.UpdateTime | datetime}}"></v-card-subtitle>
                 <v-img
-                    :src="card.src"
+                    :src="dirPath(card.FilePath)" alt="アバター画像"
                     height="200px"
-                >
-                </v-img>
-                <v-card-subtitle v-text="card.subtitle"></v-card-subtitle>
+                />
+                <v-card-subtitle v-text="{{ card.UpdateTime }}"></v-card-subtitle>
                 <v-card-actions>
                     <v-spacer />
                     <v-btn icon>
@@ -33,18 +35,52 @@
 </template>
 <script lang="ts">
 import Vue from 'vue'
+import axios from 'axios'
+
 export default Vue.extend({
   props: {
-      
+      cards: {
+          type: Array,
+          default: () => []
+      }
   },
   data() {
     return {
-      cards: [
+      cardsTest: [
         { subtitle: '#1 2021/12/25 13:49', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 6 },
         { subtitle: '#2 2021/12/25 13:50', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
         { subtitle: '#3 2021/12/25 13:51', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
       ]
     }
   },
+  filters: {
+      /*dirPath: function(val:string) {
+          if (!val) return '';
+          return  '~/' + val.replace(/\\/g, "/");
+      }*/
+      datetime: function(val:string) {
+        if (!val) return;
+        console.log(val);
+        //console.log(typeof datetime);
+        //const date = new Date(val * 1000);
+        const yyyy = `${val.getFullYear()}`;
+        // .slice(-2)で文字列中の末尾の2文字を取得する
+        // `0${date.getHoge()}`.slice(-2) と書くことで０埋めをする
+        //const MM = `0${date.getMonth() + 1}`.slice(-2); // getMonth()の返り値は0が基点
+        //const dd = `0${date.getDate()}`.slice(-2);
+        //const HH = `0${date.getHours()}`.slice(-2);
+        //const mm = `0${date.getMinutes()}`.slice(-2);
+        //const ss = `0${date.getSeconds()}`.slice(-2);
+
+        //return `${yyyy}年${MM}月${dd}日 ${HH}:${mm}:${ss}`;
+        return `11年';
+      }
+  },
+  methods: {
+      dirPath: function(val:string) {
+          if (!val) return '';
+          return  '_nuxt/' + val.replace(/\\/g, "/");
+      }
+  }
 })
 </script>
--- layouts/Top.vue
+++ layouts/Top.vue
@@ -13,7 +13,8 @@
     />
     <v-main>
         <v-container>
-            <LinkCards />
+            <LinkCards :cards=albums />
+            [{{ albums }}]
         </v-container>
     </v-main>
     <Footer />
@@ -22,12 +23,39 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import axios from 'axios'
 
 export default Vue.extend({
   middleware: 'authenticated',
+  async fetch () {
+        const authToken = this.$store.getters.getAuthToken;
+        console.log('TopページのautoToken:', authToken);
+        await axios.get('http://localhost:3010/allShareData', {
+            headers: {
+                Authorization: `token ${authToken}`
+            }
+        }).then(response => {
+
+            if (response.data.status == 'ok') {
+                console.log('apiは通信できた');
+                console.log('response.data:', response.data);
+                (this as any).albums = response.data.items;
+            }
+
+            if (response.data.status == 'NotFount') {
+                console.log('1件もデータない');
+                return { albums: '1件もデータない' };
+            }
+
+        }).catch(error => {
+            console.log('取得前にエラーが発生');
+            //this.internalFailure = true;
+        });
+  },
   data() {
     return {
-        drawer: false
+        drawer: false,
+        albums: null,
     }
   }
 })
