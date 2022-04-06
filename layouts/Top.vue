6f36ea9db59e0262839984c42ba05da9ba80bc6b
Rika Omata <omt2266@gmail.com>
2022-04-06 23:31:30 +0900

    無限スクロール表示テスト

--- layouts/Top.vue
+++ layouts/Top.vue
@@ -13,14 +13,46 @@
     />
     <v-main>
         <v-container>
-            <LinkCards :cards=albums />
-            [{{ albums }}]
+            <!--[{{ albums }}]
+            [{{ maxCount }}]
+            [{{ offset }}]-->
+            
+            <div
+              v-scroll="handleScroll"
+              class="box"
+            >
+              <!--<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A atque amet harum aut ab veritatis earum porro praesentium ut corporis. Quasi provident dolorem officia iure fugiat, eius mollitia sequi quisquam.</p>
+              <div 
+                v-for="i in offset"
+                style="width: 100px; height: 100px; background: #eee; margin-bottom:10px;"
+              >
+                [{{i}}]
+              </div>-->
+              <LinkCards :cards=albums />
+              <div v-if="loading">
+                <!--<p>Loading....</p>-->
+                <v-progress-circular
+                  indeterminate
+                 />
+              </div>
+              <div v-if="completeFlag">
+                <p>検索データがもうありません。</p>
+              </div>
+            </div> 
         </v-container>
     </v-main>
     <Footer />
   </v-app>
 </template>
+<style lang="scss">
+body {
+  height: 2000px;
+}
 
+.box {
+  transition: 1.5s all cubic-bezier(0.39, 0.575, 0.565, 1);
+}
+</style>
 <script lang="ts">
 import Vue from 'vue'
 import axios from 'axios'
@@ -30,7 +62,7 @@ export default Vue.extend({
   async fetch () {
         const authToken = this.$store.getters.getAuthToken;
         console.log('TopページのautoToken:', authToken);
-        await axios.get('http://localhost:3010/allShareData', {
+        await axios.get('http://localhost:3010/allShareData/0', {
             headers: {
                 Authorization: `token ${authToken}`
             }
@@ -41,8 +73,10 @@ export default Vue.extend({
                 for (let i = 0; i < list.length; i++) {
                     list[i].FilePath = list[i].FilePath.replace(/\\/g, "/");
                 }
-                (this as any).albums = response.data.items;
+                (this as any).albums = [...response.data.items];
+                (this as any).maxCount = parseInt(response.data.maxCount[0].COUNT);
             }
+            
 
             if (response.data.status == 'NotFount') {
                 console.log('1件もデータない');
@@ -58,6 +92,81 @@ export default Vue.extend({
     return {
         drawer: false,
         albums: null,
+        currentOffsetY: 0,
+        offset: 0,
+        loading: false,
+        maxCount: 0,
+        completeFlag: false,
+    }
+  },
+  mounted() {
+    window.scrollTo(0, 0);
+    this.completeFlag = false;
+  },
+  methods: {
+    handleScroll: function(env:any, el:any) {
+      
+      //console.log('handlescroll');
+      /*if (window.scrollY > 50) {
+        el.setAttribute(
+          'style',
+          'opacity: 1; transform: translate3d(0, -10px, 0)'
+        )
+      }
+      return window.scrollY > 100
+      */
+      
+
+      if (this.completeFlag) return;
+      
+      if (window.pageYOffset >= this.currentOffsetY) {
+        console.log('loading');
+        this.currentOffsetY = (window.innerHeight + window.pageYOffset) / 2;
+
+        if (this.offset <= this.maxCount) {
+          this.getContents();
+        } else {
+          this.completeFlag = true;
+        }
+      }
+    },
+    getContents() {
+      this.loading = true;
+
+      setTimeout(() => {
+        if (this.offset <= this.maxCount) {
+          this.offset += 1; //<-offset
+        }
+        
+        this.loading = false;
+        const authToken = this.$store.getters.getAuthToken;
+
+        axios.get(`http://localhost:3010/allShareData/${this.offset}`, {
+            headers: {
+                Authorization: `token ${authToken}`
+            }
+        }).then(response => {
+
+            if (response.data.status == 'ok') {
+                const list = response.data.items;
+                for (let i = 0; i < list.length; i++) {
+                    list[i].FilePath = list[i].FilePath.replace(/\\/g, "/");
+                }
+                (this as any).albums.push(...response.data.items);
+                (this as any).maxCount = parseInt(response.data.maxCount[0].COUNT);
+            }
+
+            if (response.data.status == 'NotFount') {
+                console.log('1件もデータない');
+                return { albums: '1件もデータない' };
+            }
+
+        }).catch(error => {
+            console.log('取得前にエラーが発生');
+            //this.internalFailure = true;
+        });
+      
+      }, 1500)
     }
   }
 })
