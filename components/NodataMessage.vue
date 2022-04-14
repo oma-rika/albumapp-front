778b66783ad795b82546806bd7f7f3bd9e08b4d0
Rika Omata <omt2266@gmail.com>
2022-04-14 20:12:40 +0900

    該当データがない場合に通知するコンポーネント追加

---
+++ components/NodataMessage.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-row justify="center">
+        <v-col
+            cols="12"
+            align="center"
+            v-if="noDataFlag"
+            class="mt-10"
+        >
+            <v-alert
+                prominent
+                type="error"
+                outlined
+                class="pt-20"
+                max-width="600"
+            >
+                <v-row align="center">
+                    <v-col class="grow">
+                        1件もまだ登録されていません。まずは編集画面から登録してください。
+                    </v-col>
+                    <v-col class="shrink">
+                        <v-btn
+                            depressed
+                            color="error"
+                            to="/dashboard"
+                        >
+                            Access dashboard
+                        </v-btn>
+                    </v-col>
+                </v-row>
+            </v-alert>
+        </v-col>
+    </v-row>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    props: {
+        noDataFlag: {
+            type: Boolean,
+            default: false
+        }
+    }
+})
+</script>
