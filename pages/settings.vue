d66513d1753fb4b2f48d61dbf032e7ffbd059c94
Rika Omata <omt2266@gmail.com>
2022-07-23 16:12:48 +0900

    設定ページ調整

--- pages/settings.vue
+++ pages/settings.vue
@@ -20,6 +20,20 @@
                         <v-tab>アクティビティ</v-tab>
                         <!-- プロフィール -->
                         <v-tab-item>
+                            <v-fade-transition>
+                                <!-- アバター登録成功メッセージ -->
+                                <v-alert
+                                    dense
+                                    text
+                                    type="success"
+                                    class="mt-12"
+                                    max-width="600"
+                                    transition="fade-transition"
+                                    v-if="avatarChageSuccessFlag"
+                                >
+                                    アイコンを更新しました
+                                </v-alert>
+                            </v-fade-transition>
                             <v-card flat>
                                 <v-card-title>
                                     プロフィール
@@ -106,6 +120,7 @@
                                                             color="blue darken-1"
                                                             text
                                                             type="submit"
+                                                            @click="adminAvatar"
                                                             :disabled="!avatarFilename"
                                                         >
                                                         Save
@@ -115,33 +130,6 @@
                                             </v-card>
                                         </v-dialog>
 
-                                        <!--<v-form
-                                            ref="form"
-                                            v-model="isValid"
-                                            @submit.prevent="adminAvatar"
-                                        >-->
-                                            <!--<v-badge
-                                                bordered
-                                                bottom
-                                                color="deep-purple accent-4"
-                                                icon="mdi-camera"
-                                                overlap
-                                                offset-x="30"
-                                                offset-y="30"
-                                            >-->
-                                                    <!--<v-file-input
-                                                        v-model="avatarFilename"
-                                                        accept="image/*"
-                                                        filled
-                                                        prepend-icon="mdi-camera"
-                                                        type="file"
-                                                        hide-input
-                                                        color="deep-purple"
-                                                        @change="adminAvatar"
-                                                        name="file"
-                                                    ></v-file-input>-->
-                                            <!--</v-badge>-->
-                                        <!--</v-form>-->
                                         <v-card>
                                             <v-card-title>ユーザー情報の変更</v-card-title>
                                             <v-card-subtitle>プロフィール情報を更新する場合はこちらから変更してください。</v-card-subtitle>
@@ -184,127 +172,11 @@
                         </v-tab-item>
                         <!-- パスワード -->
                         <v-tab-item>
-                            <v-fade-transition>
-                                <!-- 送信成功メッセージ -->
-                                <v-alert
-                                    dense
-                                    text
-                                    type="success"
-                                    class="mt-12"
-                                    max-width="600"
-                                    transition="fade-transition"
-                                    v-if="passwordChageSuccessFlag"
-                                >
-                                    パスワードの更新に成功しました。
-                                </v-alert>
-                                <!-- エラーメッセージ -->
-                                <v-alert
-                                    dense
-                                    text
-                                    type="error"
-                                    class="mt-12"
-                                    max-width="600"
-                                    transition="fade-transition"
-                                    v-if="serverErrorFlag"
-                                >
-                                    パスワードの更新に失敗しました。
-                                </v-alert>
-                            </v-fade-transition>
-                            <v-card flat>
-                                <v-card-title>
-                                    パスワード
-                                </v-card-title>
-                                <v-card-text>
-                                    <v-divider classs="mb-4" />
-                                    <v-row 
-                                        class="d-flex justify-center mt-4"
-                                        align="center">
-                                        <v-card
-                                            max-width="600"
-                                        >
-                                            <v-card-title>
-                                                現在のパスワードを変更
-                                            </v-card-title>
-                                            <v-card-subtitle>
-                                                パスワードを変更するには、必要な項目を記入して更新してください。
-                                            </v-card-subtitle>
-                                            <v-card-text>
-                                                <v-form 
-                                                    ref="form"
-                                                    v-model="isValid"  
-                                                >
-                                                    <p class="mb-3">現在のパスワード</p>
-                                                    <UserFormPassword
-                                                        :password.sync="params.password.current"
-                                                    />
-                                                    <p class="mb-3">新しいパスワード</p>
-                                                    <UserFormPassword
-                                                        :password.sync="params.password.new"
-                                                        set-validation
-                                                    />
-                                                    <p class="mb-3">新しいパスワードの確認</p>
-                                                    <v-text-field
-                                                        v-model="params.password.confirm"
-                                                        :rules="confirmPasswordRules"
-                                                        :append-icon="toggle.icon"
-                                                        outlined
-                                                        autocomplete="on"
-                                                        :type="toggle.type"
-                                                        label="パスワードを入力"
-                                                        @click:append="show = !show"
-                                                    />
-                                                    <v-btn
-                                                        :disabled="!isValid || loading"
-                                                        :loading="loading"
-                                                        block
-                                                        class="white--text"
-                                                        color="primary"
-                                                        @click="passwordChange"
-                                                    >
-                                                        更新する
-                                                    </v-btn>
-                                                </v-form>
-                                            </v-card-text>
-                                        </v-card>
-                                    </v-row>
-                                </v-card-text>
-                            </v-card>
+                            <SettingsPasswordChange />
                         </v-tab-item>
                         <!-- アクティビティ -->
                         <v-tab-item>
-                            <v-card flat>
-                                <v-card-title>
-                                    アクティビティ
-                                </v-card-title>
-                                <v-card-text>
-                                        <v-divider classs="mb-4" />
-                                        <v-card class="mt-4">
-                                            <v-simple-table>
-                                                <template v-slot:default>
-                                                    <thead>
-                                                        <tr>
-                                                            <th class="text-left">
-                                                                変更内容
-                                                            </th>
-                                                            <th class="text-left">
-                                                                日時
-                                                            </th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody>
-                                                    <tr
-                                                        v-for="item in logs"
-                                                        :key="item.name"
-                                                    >
-                                                        <td>{{ item.message }}</td>
-                                                        <td>{{ item.updateTime | datetime }}</td>
-                                                    </tr>
-                                                    </tbody>
-                                                </template>
-                                            </v-simple-table>
-                                        </v-card>
-                                </v-card-text>
-                            </v-card>
+                            <SettingsActivitylogs :logMessage="logs" />
                         </v-tab-item>
                     </v-tabs>
                 </v-container>
@@ -342,52 +214,15 @@ export default Vue.extend({
                     name: '',
                     email: '',
                     password: ''
-                },
-                password: {
-                    current: '',
-                    new: '',
-                    confirm: ''
                 }
             },
             dialog: false,
             avatarBinaryFile: '',
             drawer: false,
-            show: false,
-            passwordChageSuccessFlag: false,
             serverErrorFlag: false,
             loading: false,
             logs: null,
-        }
-    },
-    computed: {
-        //新しいパスワードと確認用に入力した値が一致するか確認
-        confirmPasswordRules () {
-            return [
-                (value :string) => !!value || '',
-                (value :string) => value === (this as any).params.password.new || '新しいパスワードと一致させてください'
-            ];
-        },
-        //パスワードの表示非表示
-        toggle (): any {
-            const icon = this.show ? 'mdi-eye' : 'mdi-eye-off'
-            const type = this.show ? 'text' : 'password'
-            return { icon, type }
-        }
-    },
-    filters: {
-        datetime: function(val:any) {
-            if (!val) return;
-            const date = new Date(val);
-            const yyyy = date.getFullYear();
-            console.log(yyyy);
-            // 0埋めにする
-            const MM = `0${date.getMonth() + 1}`.slice(-2);
-            const dd = `0${date.getDate()}`.slice(-2);
-            const HH = `0${date.getHours()}`.slice(-2);
-            const mm = `0${date.getMinutes()}`.slice(-2);
-            const ss = `0${date.getSeconds()}`.slice(-2);
-            
-            return `${yyyy}年${MM}月${dd}日 ${HH}:${mm}:${ss}`;
+            avatarChageSuccessFlag: false, //アバターの登録成功
         }
     },
     methods: {
@@ -432,6 +267,7 @@ export default Vue.extend({
                 formData.append('file', this.avatarFilename);
                 axios.post(`http://localhost:3010/avatarUpload/${userId}`, formData)
                 .then(response => {
+                    this.adminAvatarSuccess();
                     console.log({response});
                 }).catch(error => {
                     console.log({error});
@@ -450,67 +286,11 @@ export default Vue.extend({
                 this.avatarBinaryFile = '';
             }
         },
-        //パスワードの更新を実行する
-        passwordChange() {
-            if((this as any).$refs.form.validate()) {
-                const authToken = this.$store.getters.getAuthToken;
-                const p = {
-                    currentPassword: this.params.password.current,
-                    newPassword: this.params.password.new
-                };
-                axios.post('http://localhost:3010/passwordChange', p, {
-                    headers: {
-                       Authorization:  `token ${authToken}` 
-                    }
-                }).then(response => {
-                    //console.log('成功');
-                    //this.passwordChageSuccessFlag = true;
-                    this.passwordChangeSuccess();
-                }).catch(error => {
-                    this.passwordChangeFailure(error);
-                });
-                this.loading = true;
-                setTimeout(() => {
-                    this.pwformReset();
-                    this.loading = false
-                }, 2500);
-            }
-        },
-        //パスワードの入力値を初期化する
-        pwformReset() {
-            (this as any).$refs.form.reset();
-            this.params = {
-                user: {
-                    name: '',
-                    email: '',
-                    password: ''
-                },
-                password: {
-                    current: '',
-                    new: '',
-                    confirm: ''
-                }
-            };
-        },
-        //パスワードの更新に失敗した場合の処理
-        passwordChangeFailure({response}:{response:any}) {
-            //console.log('送信失敗')
-            //console.log('response:', response);
-            if (response && response.status === 403 || response && response.status === 500) {
-                console.log('FlagOON');
-                this.serverErrorFlag = true;
-                setTimeout(() => {
-                    this.serverErrorFlag = false;
-                }, 2500);
-                
-            }
-        },
-        //パスワード更新に成功した場合の処理
-        passwordChangeSuccess() {
-            this.passwordChageSuccessFlag = true;
+        //アバター画像登録に成功した場合の処理
+        adminAvatarSuccess() {
+            this.avatarChageSuccessFlag = true;
             setTimeout(() => {
-                this.passwordChageSuccessFlag = false;
-                this.$router.push('signin');
+                this.avatarChageSuccessFlag = false;   
             }, 1500);
         }
     }
