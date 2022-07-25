bd941c2af8e07c82b2f19936648439688cd99495
Rika Omata <omt2266@gmail.com>
2022-07-25 20:08:57 +0900

    お気に入り一覧ページの修正

--- pages/favorite.vue
+++ pages/favorite.vue
@@ -13,6 +13,29 @@
         />
         <v-main>
             <v-container fluid>
+                <!-- 更新完了メッセージ -->
+                <v-alert
+                dense
+                type="success"
+                class="mx-auto mt-10"
+                max-width="600"
+                transition="fade-transition"
+                v-if="UploadSuccessFlag"
+                >
+                    更新しました
+                </v-alert>
+                <!-- 更新失敗メッセージ -->
+                <v-alert
+                    dense
+                    outlined
+                    type="error"
+                    class="mx-auto mt-10"
+                    max-width="600"
+                    transition="fade-transition"
+                    v-if="UploadFailureFlag"
+                >
+                    更新失敗しました
+                </v-alert>
                 <v-row justify="center">
                     <v-col
                         cols="12"
@@ -28,16 +51,7 @@
                         >
                             <v-row align="center">
                                 <v-col class="grow">
-                                1件もまだ登録されていません。まずは編集画面から登録してください。
-                                </v-col>
-                                <v-col class="shrink">
-                                    <v-btn
-                                        depressed
-                                        color="error"
-                                        to="/dashboard"
-                                    >
-                                        Access dashboard
-                                    </v-btn>
+                                    お気に入りに保存しているアイテムはありません
                                 </v-col>
                             </v-row>
                         </v-alert>
@@ -72,30 +86,17 @@
                                 alt="photo"
                                 height="200px"
                             />
+                            <v-card-subtitle>{{ card.UpdateTime | datetime }}</v-card-subtitle>
                             <v-card-actions>
-                                    <FavoriteButton
-                                        v-bind:shared="card.favorite"
-                                        v-on:click="updateFavoriteFlag($event, card)"
-                                    />
-                                    <v-btn 
-                                        icon
-                                    >
+                                    <v-spacer />
+                                    <v-btn icon>
                                         <v-icon
-                                            :color="card.PublicFlag ? 'primary' : ''"
+                                            color='red'
+                                            @click="updateFavoriteFlag(card)"
                                         >
-                                            mdi-account-plus
+                                            mdi-heart
                                         </v-icon>
                                     </v-btn>
-                                    <v-btn 
-                                        icon
-                                    >
-                                        <v-icon
-                                            :color="card.PublicFlag ? 'orange' : ''"
-                                        >
-                                            mdi-trash-can
-                                        </v-icon>
-                                    </v-btn>
-                                </v-btn>
                             </v-card-actions>
                         </v-card>
                     </v-col>
@@ -138,6 +139,24 @@ export default Vue.extend({
             albums: null,
             NothingFlag: false,
             internalFailure: false,
+            UploadSuccessFlag: false,
+            UploadFailureFlag: false,
+        }
+    },
+    filters: {
+        datetime: function(val:any) {
+            if (!val) return;
+            const date = new Date(val);
+            const yyyy = date.getFullYear();
+            console.log(yyyy);
+            // 0埋めにする
+            const MM = `0${date.getMonth() + 1}`.slice(-2);
+            const dd = `0${date.getDate()}`.slice(-2);
+            const HH = `0${date.getHours()}`.slice(-2);
+            const mm = `0${date.getMinutes()}`.slice(-2);
+            const ss = `0${date.getSeconds()}`.slice(-2);
+            
+            return `${yyyy}年${MM}月${dd}日 ${HH}:${mm}:${ss}`;
         }
     },
     methods: {
@@ -145,12 +164,12 @@ export default Vue.extend({
             let imagePath = url.replace(/\\/g, "/");
             return `appData/${imagePath}`;
         },
-        async updateFavoriteFlag(event:any, card:any) {
+        async updateFavoriteFlag(card:any) {
             card.favorite = event;
             const param = {
-                id: card.id,
-                favorite: event
+                fileId: card.ID,
             };
+            console.log('param:', param);
             const authToken = this.$store.getters.getAuthToken;
             await axios.post('http://localhost:3010/favorite/', param, {
                 headers: {
@@ -163,10 +182,19 @@ export default Vue.extend({
             });
         },
         uploadSuccessful (response:any) {
-            console.log('成功した');
+            //console.log('成功した');
+            this.UploadSuccessFlag = true;
+            setTimeout(() => {
+                this.UploadSuccessFlag = false;
+                location.reload();
+            }, 1500);    
         },
         uploadFailure (error: AxiosError<{error: string}>) {
-            console.log('失敗した');
+            //console.log('失敗した');
+            this.UploadFailureFlag = true;
+            setTimeout(() => {
+                this.UploadFailureFlag = false;
+            }, 1500);
         },
     }
 })
